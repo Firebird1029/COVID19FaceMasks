c3180829349bfe87116c58d223e5d9f9f327b92a
Brandon Yee <brandon.yee150@gmail.com>
2020-04-17 19:44:43 -1000

    Initial Commit

---
+++ src/js/client.js
@@ -0,0 +1,11 @@
+"use strict"; /* eslint-env browser */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
+
+// Navbar Burger
+$(document).ready(function () {
+	$(".navbar-burger").click(function () {
+		$(".navbar-burger").toggleClass("is-active");
+		$(".navbar-menu").toggleClass("is-active");
+	});
+});
+
+// var socket = io.connect();
