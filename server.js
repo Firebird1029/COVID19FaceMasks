c9be0e5e284d97d1980228cfffacffe6276699d2
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2020-05-30 11:24:07 -1000

    Added Sendgrid

--- server.js
+++ server.js
@@ -52,3 +52,22 @@ db.once("open", (_) => {
 db.on("error", (err) => {
 	debug && console.error("Database connection error:", err);
 });
+
+// using Twilio SendGrid's v3 Node.js Library -- TODO
+// https://github.com/sendgrid/sendgrid-nodejs
+// const sgMail = require("@sendgrid/mail");
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+// 	to: "brandon.yee150@gmail.com",
+// 	from: "byee20@punahou.edu",
+// 	subject: "Sending with Twilio SendGrid is Fun",
+// 	text: "and easy to do anywhere, even with Node.js",
+// 	html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+// };
+// sgMail
+// 	.send(msg, (err, result) => {
+// 		console.log("err1", JSON.stringify(err));
+// 		console.log("result1", result);
+// 	})
+// 	.catch((err) => console.log("err", err));
+// console.log(process.env.SENDGRID_API_KEY);
